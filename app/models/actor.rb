ddece4e9684e29f862d1c53b820554dad8060443
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-11-07 22:28:39 +0300

    complete

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
+  
+    def full_name
+      "#{self.first_name} #{self.last_name}"
+    end
+  
+    def list_roles
+      self.characters.count.times.map do |index|
+        "#{self.characters[index].name} - #{self.shows[index].name}"
+      end
+    end
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
   
-end
+    def actors_list 
+      self.actors.count.times.map do |index|
+        "#{self.actors[index].first_name} #{self.actors[index].last_name}"
+      end
+    end
+  end
